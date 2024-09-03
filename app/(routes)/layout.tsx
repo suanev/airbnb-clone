bbbb08483d6c0e368e63d0c51c29044b36e8b949
Suane Vallim <suane.vallim@ilegra.com>
2024-09-03 17:15:31 -0300

    first commit

---
+++ app/(routes)/layout.tsx
@@ -0,0 +1,42 @@
+import { Nunito } from "next/font/google";
+import Navbar from "@/app/layout/navbar";
+import RegisterModal from "@/app/layout/modals/register";
+import ToasterProvider from "@/app/providers/ToasterProvider";
+import LoginModal from "@/app/layout/modals/login";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import RentModal from "@/app/layout/modals/rent";
+import ClientOnly from "@/app/components/client-only";
+import SearchModal from "@/app/layout/modals/search";
+import "../globals.css";
+
+export const metadata = {
+  title: "Airbnb by Suane",
+  description: "Airbnb clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const currentUser = await getCurrentUser();
+  return (
+    <html lang="en">
+      <body className={font.className} suppressHydrationWarning={true}>
+        <ClientOnly>
+          <ToasterProvider />
+          <LoginModal />
+          <RegisterModal />
+          <RentModal />
+          <SearchModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className="pb-20 pt-28">{children}</div>
+      </body>
+    </html>
+  );
+}
